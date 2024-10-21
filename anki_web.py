3a43fff0accd66f9c1e5391f252e5aea978f1667
xieyifan <yifan.xie@foxmail.com>
2024-10-21 18:14:59 +0800

    feat: 添加 AnkiWeb 登录保活和卡片信息获取功能，抽象部分方法为公共函数
    - 新增 anki_web.py 文件，实现 AnkiWeb 账号登录和卡片主页信息获取
    - 添加 script_tools.py 文件，提供基础运行工具和日志记录功能
    - 实现了环境变量解析、随机延迟运行、通知服务加载等通用功能
    - 采用面向对象的设计，便于扩展和维护

---
+++ anki_web.py
@@ -0,0 +1,75 @@
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+import traceback
+import requests
+urllib3.disable_warnings(InsecureRequestWarning)
+from script_tools import BaseRun
+import os
+
+class Run(BaseRun):
+
+    def init_vars(self):
+        self.headers = {
+            "Content-Type": "application/octet-stream"
+        }
+        self.base_url = "https://ankiweb.net/"
+        self.session = requests.Session()
+    def process_var(self, info):
+        username, password = info.split(";")
+        text = f"{username}{password}".encode()
+        text_array = bytearray(text)
+        text_array.insert(0,17)
+        text_array.insert(0,10)
+        text_array.insert(19,6)
+        text_array.insert(19,18)
+        self.run_token = text_array
+
+    def login(self):
+        self.session.headers.update(self.headers)
+        url = f"{self.base_url}svc/account/login"
+        login_result = self.session.post(url, data=self.run_token)
+        if login_result.status_code == 200:
+            temp_refresh_token = login_result.text[5:]
+            confirm_login_result = self.session.get(f"https://ankiuser.net/account/ankiuser-login?t={temp_refresh_token}")
+            if confirm_login_result.status_code == 200:
+                self.logger.info("✅ 登录成功！")
+                return True
+        self.logger.error("❌ 登录失败！！")
+        return False
+    def check_deck_list(self):
+        url = f"{self.base_url}svc/decks/deck-list-info"
+        data = bytearray([8,160,252,255,255,255,255,255,255,1])
+        deck_list_result = self.session.post(url,data=data)
+        if deck_list_result.status_code == 200:
+            self.logger.info("✅ 获取卡片主页信息成功")
+        else:
+            self.logger.error("❌ 获取卡片主页信息失败")
+    def process(self):
+        self.logger.info(f"当前版本：{local_version}")
+        if self.app_env_infos:
+            self.logger.info(f"\n=======\t共获取到 {len(self.app_env_infos)} 个账号")
+        else:
+            return
+        for index, info in enumerate(self.app_env_infos):
+            try:
+                self.logger.info(f"\n=======\t开始执行第 {index + 1} 个账号")
+                self.logger.info(f"\n==> 处理账号密码")
+                self.process_var(info)
+                self.logger.info(f"\n==> 登录 AnkiWeb 账号")
+                if self.login():
+                    self.logger.info(f"\n==> 测试登录状态")
+                    self.check_deck_list()
+                return True
+            except Exception:
+                self.logger.error(traceback.format_exc())
+                return False
+
+
+if __name__ == "__main__":
+    app_name = "AnkiWeb"
+    app_env_name = "ANKIWEB_CREDENTIALS"
+    local_script_name = os.path.basename(__file__)
+    local_version = '2024.09.23'
+    run = Run(app_name=app_name,
+              app_env_name=app_env_name)
+    run.main()
---
+++ script_tools.py
@@ -0,0 +1,125 @@
+import random
+import time
+from enum import Enum
+import os
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+from abc import ABC, abstractmethod
+
+urllib3.disable_warnings(InsecureRequestWarning)
+
+class BaseRun(ABC):
+    def __init__(self, app_name=None, app_env_name=None, app_env_infos=None):
+        self.logger = self.ScriptLogger()
+        self.tools = self.ScriptTool()
+        self.app_name = app_name
+        self.app_env_name = app_env_name
+        self.app_env_infos = app_env_infos
+
+
+
+    class ScriptTool:
+        @staticmethod
+        def env_split(env_str):
+            parts = []
+            if '&' in env_str:
+                amp_parts = env_str.split('&')
+                for part in amp_parts:
+                    if '#' in part:
+                        hash_parts = part.split('#')
+                        for hash_part in hash_parts:
+                            parts.append(hash_part)
+                    else:
+                        parts.append(part)
+                return (parts)
+            elif '#' in env_str:
+                hash_parts = env_str.split('#')
+                return (hash_parts)
+            else:
+                out_str = str(env_str)
+                return ([out_str])
+        @staticmethod
+        def random_delay_run(min_delay=60, max_delay=120):
+            delay = random.uniform(min_delay, max_delay)
+            print(f"\n随机延迟{delay}秒\n")
+            time.sleep(delay)
+        @staticmethod
+        def load_send():
+            cur_path = os.path.abspath(os.path.dirname(__file__))
+            notify_file = cur_path + "/notify.py"
+            if os.path.exists(notify_file):
+                try:
+                    from notify import send  # 导入模块的send为notify_send
+                    print("加载通知服务成功！")
+                    return send  # 返回导入的函数
+                except ImportError:
+                    print("加载通知服务失败~")
+            else:
+                print("加载通知服务失败~")
+            return False
+        @staticmethod
+        def dev():
+            cur_path = os.path.abspath(os.path.dirname(__file__))
+            env_file = cur_path + "/.env"
+            if os.path.isfile(env_file):
+                from dotenv import load_dotenv
+                load_dotenv(env_file)
+                return True
+            else:
+                return False
+
+    class ScriptLogger:
+        class ScriptStatus(Enum):
+            SUCCESS = "正常"
+            FAILURE = "异常"
+        '''日志记录器，用于记录日志并获取通知消息'''
+        def __init__(self):
+            self.notify_message = ""
+            self.status = self.ScriptStatus.SUCCESS
+        def print(self, one_message):
+            if one_message:
+                self.notify_message += f"{one_message}\n"
+                print(f"{one_message}")
+        def info(self, one_message):
+            self.print(one_message)
+        def error(self, one_message):
+            self.status = self.ScriptStatus.FAILURE
+            self.print(one_message)
+
+
+        def get_notify_message(self):
+            return self.notify_message
+        def get_status(self):
+            return self.status
+    @abstractmethod
+    def init_vars(self, index, info):
+        """初始化数据，必须由子类实现"""
+        pass
+
+    @abstractmethod
+    def process(self):
+        """处理数据，必须由子类实现"""
+        pass
+    def notify(self):
+        """发送通知，这是一个钩子方法，可以选择重写"""
+        send = self.ScriptTool.load_send()
+        if send:
+            send(f'{self.app_name}挂机通知【{self.logger.status.value}】', self.logger.notify_message)
+    def get_envinfo(self):
+        if self.app_env_infos:
+            return
+        app_env_str = os.environ.get(self.app_env_name)
+        if not app_env_str:
+            self.logger.error(f"未填写 {self.app_env_name} 变量, 青龙可在环境变量设置 {self.app_env_name}")
+        else:
+            app_envs = self.tools.env_split(app_env_str)
+            if len(app_envs) > 0:
+                self.app_env_infos = app_envs
+    # 模板方法，定义算法的框架
+    def main(self):
+        if not self.tools.dev():
+            self.tools.random_delay_run()
+        self.get_envinfo()
+        self.init_vars()
+        self.process()
+        self.notify()
